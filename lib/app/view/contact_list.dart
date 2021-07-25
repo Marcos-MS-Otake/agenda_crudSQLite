c4346fdf7e90b57bd982d31b8aea33860104aab4
marcos_otake <masatiro111@hotmail.com>
2021-07-25 08:12:10 -0300

    criação de botões editar e deletar na lista

--- lib/app/view/contact_list.dart
+++ lib/app/view/contact_list.dart
@@ -6,6 +6,26 @@ class ContactList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final lista = [
+      {
+        'nome': 'Pietro',
+        'telefone': '(11)9 9956-3621',
+        'avatar':
+            'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+      },
+      {
+        'nome': 'Maite',
+        'telefone': '(11)9 9953-3333 ',
+        'avatar':
+            'https://cdn.pixabay.com/photo/2021/07/08/07/23/child-6396103_960_720.png'
+      },
+      {
+        'nome': 'Hortencia',
+        'telefone': '(11)9 9666-6666',
+        'avatar':
+            'https://cdn.pixabay.com/photo/2021/07/11/07/12/woman-6403071_960_720.png'
+      }
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Contato'),
@@ -18,7 +38,30 @@ class ContactList extends StatelessWidget {
           )
         ],
       ),
-      body: ListView(),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: lista.length,
+        itemBuilder: (context, index) {
+          var contato = lista[index];
+          var avatar = CircleAvatar(
+            backgroundImage: NetworkImage(contato['avatar']),
+          );
+          return ListTile(
+            leading: avatar,
+            title: Text(contato['nome']),
+            subtitle: Text(contato['telefone']),
+            trailing: Container(
+              width: 100,
+              child: Row(
+                children: [
+                  IconButton(icon: Icon(Icons.edit), onPressed: null),
+                  IconButton(icon: Icon(Icons.delete), onPressed: null)
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
